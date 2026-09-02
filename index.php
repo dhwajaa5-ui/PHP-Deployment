ec941773da9d724e5f54ccf83b48504cf85cf356
Dhwaja <dhwajaa5@gmail.com>
2026-09-02 16:31:19 +0530

    code

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Azure PHP App</title>
+</head>
+
+<body>
+    <h1>Hello from Azure Web App! 🚀</h1>
+    
+    <p>My first PHP application is successfully deployed.</p>
+
+    <?php
+        echo "<h2>PHP is working successfully! ✅</h2>";
+        echo "<p>Current Server Time: " . date("Y-m-d H:i:s") . "</p>";
+    ?>
+
+</body>
+</html>
